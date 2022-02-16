6db9d51911e7b7c9f10af2bde6650c6f3e06ce78
BJTowne <bjtowne9@gmail.com>
2022-02-15 23:13:00 -0500

    Local proxy set up

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  devServer:{
+    proxy:{
+      '^/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: {'^/api' : ''}
+      },
+      '^/serverlessws': {
+        target: 'http://localhost:3001',
+        ws:true,
+        pathRewrite: {'^/serverlessws' : ''},
+        changeOrigin: true
+      }
+    }
+  },
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
