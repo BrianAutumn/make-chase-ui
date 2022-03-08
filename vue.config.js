691013aa32ee92cc7eaae82e2ed5c9b4adcce440
BJTowne <bjtowne9@gmail.com>
2022-03-08 13:29:04 -0500

    Deploy fixes

--- vue.config.js
+++ vue.config.js
@@ -4,20 +4,6 @@ module.exports = {
       '^/api': {
         target: 'http://localhost:3000',
         pathRewrite: {'^/api' : ''}
-      },
-      '^/serverlessws': {
-        target: 'http://localhost:3001',
-        ws:true,
-        pathRewrite: {'^/serverlessws' : ''},
-        changeOrigin: true,
-        onProxyReqWs(proxyReq, req, res){
-          if(req?.headers?.cookie){
-            req.headers.cookie = undefined;
-          }
-          if(proxyReq?.headers?.cookie){
-            proxyReq.headers.cookie = undefined;
-          }
-        }
       }
     }
   },
