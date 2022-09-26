4724f09820a997d488acd3c46bd3c85b61937514
BJTowne <bjtowne9@gmail.com>
2022-09-25 22:51:41 -0400

    Added manual tokens

--- src/appConf.js
+++ src/appConf.js
@@ -3,7 +3,7 @@ export const appConf = {
     clientId: "92116833711-06i78gunp707ht8vla0cdnsft3f1n5ks.apps.googleusercontent.com"
   },
   apollo: {
-    httpUrl: 'http://makechase.com/api',
-    wsUrl: 'ws://makechase.com/ws'
+    httpUrl: 'https://makechase.com/api',
+    wsUrl: 'wss://makechase.com/ws'
   }
 }
