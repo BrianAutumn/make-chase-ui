de632ef16890a6d94b5053befc5a5432bd7403c5
BJTowne <bjtowne9@gmail.com>
2022-10-10 19:13:05 -0400

    Added UI Tokens Added special google domain for local testing

--- src/appConf.js
+++ src/appConf.js
@@ -1,10 +1,12 @@
+import {getToken} from "@/token-manger";
+
 const secure = document.location.protocol.toLowerCase() === 'https:'
 const httpProtocol = secure?'https:':'http:';
 const wsProtocol = secure?'wss:':'ws:';
 
 export const appConf = {
   google: {
-    clientId: "92116833711-06i78gunp707ht8vla0cdnsft3f1n5ks.apps.googleusercontent.com"
+    clientId: getToken('GOOGLE_CLIENT_ID')
   },
   apollo: {
     httpUrl: `${httpProtocol}//${document.location.host}/api`,
---
+++ src/tokens.js
@@ -0,0 +1,6 @@
+export const tokens = {
+  GOOGLE_CLIENT_ID: {
+    token:'###GOOGLE_CLIENT_ID###',
+    local:'126288369281-utemo0o3euttgkuuut5k7gmtddm61li6.apps.googleusercontent.com'
+  }
+}
