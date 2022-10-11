535b9b799a294bda1ecd3b190ac181d7b5b45ad0
BJTowne <bjtowne9@gmail.com>
2022-10-11 13:33:22 -0400

    Fixed tokens

--- src/token-manger.js
+++ src/token-manger.js
@@ -4,12 +4,12 @@ export function getToken(name){
   if(!tokens[name]){
     throw `Token '${name}' not defined!`
   }
-  if(isSet(tokens[name].token) || !tokens[name].local){
+  if(isSet(tokens[name].token,name) || !tokens[name].local){
     return tokens[name].token
   }
   return tokens[name].local;
 }
 
-function isSet(tokenValue){
-  return !tokenValue.startsWith('##') && !tokenValue.endsWith('##')
+function isSet(tokenValue, name){
+  return tokenValue !== `##${name}##`
 }
